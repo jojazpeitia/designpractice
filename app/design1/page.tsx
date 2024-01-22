ad20c38de4d6e701a77041f2760b8ad986ff3b47
jojazpeitia <jojazpeitia@gmail.com>
2024-01-22 14:05:31 -0600

    created page specific css for design1

--- app/design1/page.tsx
+++ app/design1/page.tsx
@@ -1,7 +1,34 @@
+import styles from './styles.module.css'
+
 export default function Design1() {
-    return (
-      <div className="dots">
-        <div className="dot"></div>
+  return (
+    <div className='bg-white min-h-screen grid items-center'>
+      <div className={styles.dots}>
+        <div className={styles.dot}/>
+        <div className={styles.dot}/>
+        <div className={styles.dot}/>
+        <div className={styles.dot}/>
+        <div className={styles.dot}/>
+
+        <div className={styles.dot}/>
+        <div className={styles.dot}/>
+        <div className={styles.dot}/>
+        <div className={styles.dot}/>
+        <div className={styles.dot}/>
+
+        <div className={styles.dot}/>
+        <div className={styles.dot}/>
+        <div className={styles.dot}/>
+        <div className={styles.dot}/>
+        <div className={styles.dot}/>
+
+        <div className={styles.dot}/>
+        <div className={styles.dot}/>
+        <div className={styles.dot}/>
+        <div className={styles.dot}/>
+        <div className={styles.dot}/>
+
       </div>
-    );
-  }
+    </div>
+  );
+}
