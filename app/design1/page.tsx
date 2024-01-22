4d32f56327db23d6ad90b2c2d0f930cc522b9a8a
jojazpeitia <jojazpeitia@gmail.com>
2024-01-22 15:39:58 -0600

    finished implementing dots design

--- app/design1/page.tsx
+++ app/design1/page.tsx
@@ -1,33 +1,19 @@
 import styles from './styles.module.css'
 
 export default function Design1() {
+
+  const numberOfDots = 400;
+
+  const dotElements = [];
+
+  for (let i = 0; i < numberOfDots; i++) {
+    dotElements.push(<div key={i} className={styles.dot} />);
+  }
+
   return (
-    <div className='bg-white min-h-screen grid items-center'>
+    <div className=' min-h-screen items-center justify-center flex'>
       <div className={styles.dots}>
-        <div className={styles.dot}/>
-        <div className={styles.dot}/>
-        <div className={styles.dot}/>
-        <div className={styles.dot}/>
-        <div className={styles.dot}/>
-
-        <div className={styles.dot}/>
-        <div className={styles.dot}/>
-        <div className={styles.dot}/>
-        <div className={styles.dot}/>
-        <div className={styles.dot}/>
-
-        <div className={styles.dot}/>
-        <div className={styles.dot}/>
-        <div className={styles.dot}/>
-        <div className={styles.dot}/>
-        <div className={styles.dot}/>
-
-        <div className={styles.dot}/>
-        <div className={styles.dot}/>
-        <div className={styles.dot}/>
-        <div className={styles.dot}/>
-        <div className={styles.dot}/>
-
+        {dotElements}
       </div>
     </div>
   );
