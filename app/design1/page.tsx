3b9bb6d5852180393a94f2a17a28509dbabef9a7
jojazpeitia <jojazpeitia@gmail.com>
2024-02-13 23:55:33 -0600

    cube

--- app/design1/page.tsx
+++ app/design1/page.tsx
@@ -11,9 +11,14 @@ export default function Design1() {
   }
 
   return (
-    <div className=' min-h-screen items-center justify-center flex'>
-      <div className={styles.dots}>
-        {dotElements}
+    <div>
+      <h1 className="justify-center items-center flex mt-36">
+        dot
+      </h1>
+      <div className='items-center justify-center flex'>
+        <div className={`${styles.dots} scale-100`}>
+          {dotElements}
+        </div>
       </div>
     </div>
   );
--- app/design2/page.tsx
+++ app/design2/page.tsx
@@ -1,7 +1,40 @@
+'use client'
+
+import { Canvas, useFrame } from "@react-three/fiber"
+import { useRef } from "react";
+import { Mesh } from "three";
+
+function Cube() {
+  const meshRef = useRef<Mesh>(null);
+
+  useFrame(() => {
+    if (!meshRef.current) {
+      return;
+    }
+
+    meshRef.current.rotation.x += 0.01;
+    meshRef.current.rotation.y += 0.01;
+  });
+
+  return (
+    <mesh ref={meshRef}>
+      <boxGeometry args={[2, 2, 2]} />
+      <meshStandardMaterial color="white" />
+    </mesh>
+  );
+}
+
 export default function Design2() {
   return (
-    <div className="dots">
-      <div className="dot"></div>
+    <div>
+      <h1 className="justify-center items-center flex mt-36">
+        cube
+      </h1>
+      <Canvas>
+        <ambientLight/>
+        <pointLight position={[0,0,0]} />
+        <Cube />
+      </Canvas>
     </div>
   );
 }
